0524b4ea810f3f36d3135fb2a30c897865a3af2a
Vin2113 <87746566+Vin2113@users.noreply.github.com>
2022-05-04 19:24:26 +0700

    update

--- Systems/Database Connection.py
+++ Systems/Database Connection.py
@@ -1,22 +1,7 @@
-import mysql.connector
-connection = mysql.connector.connect(host='localhost',
-                             user='root',
-                             password='Vl1021996499.',
-                            database = 'reservation_system'
-                           )
-cursor = connection.cursor()
-query = ('SELECT email FROM customer')
-cursor.execute(query)
-lst = []
-lst = [ i[0] for i in cursor]
-# print(lst)
-cursor.execute(query)
-new_list = []
-for all in cursor:
-    new_list.append(all[0])
-# print(new_list)
+from model import my_cursor, connection
 
-query = f"SELECT email from customer WHERE email = 'one@nyu.edu'"
-cursor.execute(query)
-user = [ i[0] for i in cursor if i[0] == 'one@nyu.edu' ]
-print(user)
+query = f"SELECT email, password from customer WHERE email = 'vincent@gmail.com'"
+
+my_cursor.execute(query)
+account = my_cursor.fetchone()
+print(account)
