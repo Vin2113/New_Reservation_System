a918ba116f1c60f69b1fda48b3faf831ac1d0ae0
Picucu <fang.ethany@gmail.com>
2022-05-08 20:15:52 +0800

    Update Routes.py

--- Systems/Routes.py
+++ Systems/Routes.py
@@ -52,12 +52,14 @@ def profile():
 
 @app.route('/profile/<Username>', methods=["GET","POST"])
 def profileCust(Username):
+    data={}
     with customer_connection.cursor(pymysql.cursors.DictCursor) as mycursor:
             query = f"select F.airline_name, F.flight_num, F.departure_airport, F.departure_time, F.arrival_airport, F.arrival_time, F.price, F.status, F.airplane_id, T.ticket_id from flight as F right join ticket as T on F.flight_num=T.flight_num right join (select * from purchases where customer_email = '{session['username']}') as P on T.ticket_id = P.ticket_id"
             mycursor.execute(query)
             history = mycursor.fetchall()
-            session['history'] = history
-            print(session['history'])
+            data['history'] = history
+            query = f"select sum(F.price) from flight as F left join ticket as T on T.flight_num = F.flight_num left join purchases as P on T.ticket_id = P.ticket_id where P.customer_email = '{session['username']}' and p.purchase_date > date_sub(curdate(), INTERVAL 1 day)"
+            mycursor.execute(query)
             mycursor.close()
     return render_template('Profile.html', title='Profile')
 
